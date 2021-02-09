5a6ca6d7bb5ecfdd60fa4bb25798f399c77dc3e6
chaidbatan <chaidbatan@gmail.com>
2021-02-09 20:15:44 +0800

    update

--- AvionSchool/Projects/003/script.js
+++ AvionSchool/Projects/003/script.js
@@ -1,8 +1,19 @@
-function nav() {
-    var nav = document.getElementsByClassName("nav-icon");
-    if (nav.style.display === "block") {
-        nav.style.display = "none";
-    } else {
-        nav.style.display = "block";
-    }
+const navBar = document.querySelector('#nav-bar');
+const openMenu = document.querySelector('#open-menu');
+const closeMenu = document.querySelector('#close-menu');
+const menu = document.querySelector('.menu');
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', hide);
+
+function show() {
+    menu.style.display = 'block';
+    closeMenu.style.display = 'block';
+    openMenu.style.display = 'none';
+    menu.style.top = '0';
+}
+function hide() {
+    menu.style.display = 'none';
+    openMenu.style.display = 'block';
+    closeMenu.style.display = 'none';
 }
