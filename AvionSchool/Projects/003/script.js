c7db5f8fb937932b5481bd61155a0ea28b61e56b
chaidbatan <chaidbatan@gmail.com>
2021-02-16 18:43:09 +0800

    final proj

--- AvionSchool/Projects/003/script.js
+++ AvionSchool/Projects/003/script.js
@@ -1,20 +1,35 @@
-const navBar = document.querySelector('#nav-bar');
+// const navBar = document.querySelector('#nav-bar');
+//const openMenu = document.querySelector('#open-menu');
+//const closeMenu = document.querySelector('#close-menu');
+//const menu = document.querySelector('#header');
+
+//openMenu.addEventListener('click', show);
+//closeMenu.addEventListener('click', hide);
+
+//function show() {
+//    menu.style.display = 'block';
+//    menu.style.padding = '0';
+  //  menu.style.margin ='0';
+//    closeMenu.style.display = 'block';
+//    openMenu.style.display = 'none';
+//}
+
+const nav = document.querySelector('#nav');
 const openMenu = document.querySelector('#open-menu');
-const closeMenu = document.querySelector('#close-menu');
-const menu = document.querySelector('.menu');
+const closeMenu = document.querySelector('#close-menu')
+
 
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', hide);
 
 function show() {
-    menu.style.display = 'block';
-    menu.style.padding = '0';
-    menu.style.margin ='0';
-    closeMenu.style.display = 'block';
+    nav.style.display = 'flex';
     openMenu.style.display = 'none';
+    closeMenu.style.display = 'block';
 }
 function hide() {
-    menu.style.display = 'none';
+    nav.style.display = 'none';
     openMenu.style.display = 'block';
     closeMenu.style.display = 'none';
+
 }
