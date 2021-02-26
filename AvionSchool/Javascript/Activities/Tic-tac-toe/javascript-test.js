3ee262c73f600c1ccde5e84e2bd91aa5bb41f49b
chaidbatan <chaidbatan@gmail.com>
2021-02-26 18:54:16 +0800

    update

---
+++ AvionSchool/Javascript/Activities/Tic-tac-toe/javascript-test.js
@@ -0,0 +1,16 @@
+const grid = document.querySelectorAll(".container");
+
+const array = [];
+array[0][0] = document.getElementById('1');
+array[0][1] = document.getElementById('2');
+array[0][2] = document.getElementById('3');
+array[1][0] = document.getElementById('4');
+array[1][1] = document.getElementById('5');
+array[1][2] = document.getElementById('6');
+array[2][0] = document.getElementById('7');
+array[2][1] = document.getElementById('8');
+array[2][2] = document.getElementById('9');
+
+function myFunction() {
+    array[0][0] =
+}
---
+++ AvionSchool/Javascript/Activities/Tic-tac-toe/script.js
@@ -0,0 +1,91 @@
+const winComb = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+]
+function myFunction() {
+    winComb[0][0]='charm';
+    document.getElementById("1").innerHTML = winComb[0][0];
+}
+function myFunction1() {
+    winComb[0][1]='1';
+    document.getElementById("2").innerHTML = winComb[0][1];
+}
+function myFunction2() {
+    winComb[0][2]='charm';
+    document.getElementById("3").innerHTML = winComb[0][2];
+}
+function myFunction3() {
+    winComb[1][0]='charm';
+    document.getElementById("4").innerHTML = winComb[1][0];
+}
+function myFunction4() {
+    winComb[1][1]='charm';
+    document.getElementById("5").innerHTML = winComb[1][1];
+}
+function myFunction5() {
+    winComb[1][2]='charm';
+    document.getElementById("6").innerHTML = winComb[1][2];
+}
+function myFunction6() {
+    winComb[2][0]='charm';
+    document.getElementById("7").innerHTML = winComb[2][0];
+}
+function myFunction7() {
+    winComb[2][1]='charm';
+    document.getElementById("8").innerHTML = winComb[2][1];
+}
+function myFunction8() {
+    winComb[2][2]='charm';
+    document.getElementById("9").innerHTML = winComb[2][2];
+}
+
+let arr1 = [
+    ['1', '1', '1'],
+    ['1', '', ''],
+    ['', '', '']
+]
+let arr2 = [
+    ['2', '2', '2'],
+    ['', '', ''],
+    ['', '', '']
+]
+let arr3 = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+]
+let arr4 = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+]
+let arr5 = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+]
+let arr6 = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+]
+let arr7 = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+]
+let arr8 = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+]
+let arr9 = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+]
+
+function prev() {
+    document.getElementById("prev").innerHTML = ;
+}
