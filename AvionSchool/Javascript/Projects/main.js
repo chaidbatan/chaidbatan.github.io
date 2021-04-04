ca85aa5c5f74e972f00edb3198b7d35b6b9ce282
chaidbatan <chaidbatan@gmail.com>
2021-04-04 09:34:57 +0800

    update js project

---
+++ AvionSchool/Javascript/Projects/main.js
@@ -0,0 +1,12 @@
+// sign-in 
+function signIn() {
+    var form = document.createElement('form');
+    var label = document.createElement('label');
+    var input = document.createElement('input');
+    input.setAttribute('type',"text");
+    input.setAttribute('name',"username");
+    console.log('create');
+}
+function register() {
+
+}
